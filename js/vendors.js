386de78a4efb1bfbf712507a3817e43a085359fb
Seemant Kulleen <seemantk@gmail.com>
2016-03-22 00:09:50 -0700

    Add vendors.{js,css} which concatenate the vendors files, to minimize number of downloads per browser.

---
+++ js/vendors.js
@@ -0,0 +1,6 @@
+---
+---
+{% include_relative vendor/lodash.min.js %}
+{% include_relative vendor/queue.min.js %}
+{% include_relative vendor/topojson.min.js %}
+{% include_relative vendor/d3.min.js %}
