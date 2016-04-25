0907d05d8909d41513630de494a8c2617475c785
Seemant Kulleen <seemantk@gmail.com>
2016-04-25 14:12:25 -0700

    Migrate to d3-queue and use d3-tip.

--- js/vendors.js
+++ js/vendors.js
@@ -1,6 +1,7 @@
 ---
 ---
 {% include_relative vendor/lodash.min.js %}
-{% include_relative vendor/queue.min.js %}
 {% include_relative vendor/topojson.min.js %}
 {% include_relative vendor/d3.min.js %}
+{% include_relative vendor/d3-queue.min.js %}
+{% include_relative vendor/d3-tip.min.js %}
